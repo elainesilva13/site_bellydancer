5b8e3de607e0bdb363ca0f80cec82d0c0663ba44
Super Nani <elaine.programadora@gmail.com>
2024-11-23 19:51:44 -0300

    Ajustes SEO

--- templates/header.php
+++ templates/header.php
@@ -1,28 +1,36 @@
 <!DOCTYPE html>
-
 <html lang="pt-BR">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Elaine Noura bailarina de dança do ventre de São José dos Campos.">
+    <meta name="description"
+        content="Elaine Noura é bailarina de dança do ventre de São José dos Campos, oferecendo apresentações de danças orientais, cultura árabe e muito mais em festas e eventos.">
     <meta name="keywords"
         content="bailarina, dança do ventre, danças orientais, apresentações, cultura árabe, festas, aniversários, egito, arabia, líbano, turquia, saidi, baladi, são josé dos campos, sjc, restaurantes, entretenimento, coreografia">
-    <title>Elaine Noura Bellydancer</title>
+    <meta name="author" content="Elaine Noura">
     <meta name="robots" content="index, follow">
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <title>Elaine Noura Bellydancer | Dança do Ventre e Danças Orientais</title>
+    <link rel="icon" href="https://elainebellydancer.infinityfreeapp.com/favicon.ico" type="image/x-icon">
     <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
     <link href="css/estilo.css" rel="stylesheet">
+    <meta property="og:title" content="Elaine Noura Bellydancer | Dança do Ventre e Danças Orientais">
+    <meta property="og:description"
+        content="Elaine Noura, bailarina de dança do ventre de São José dos Campos, oferece performances de danças orientais e culturais árabes em festas e eventos.">
+    <meta property="og:image" content="https://elainebellydancer.infinityfreeapp.com/images/thumbnail.jpg">
+    <meta property="og:url" content="https://elainebellydancer.infinityfreeapp.com/">
+    <meta property="og:type" content="website">
+    <link rel="canonical" href="https://elainebellydancer.infinityfreeapp.com/">
 </head>
-<?php require_once 'templates/nav.php'; ?>
 
 <body>
+    <?php require_once 'templates/nav.php'; ?>
 
-<header>
-    <h1>Elaine Noura</h1>
-    <p>Dança do Ventre, Danças Folclóricas Árabes e mais.</p>
-    <div class="stars" id=stars>
-    </div>
-</header>
+    <header>
+        <h1>Elaine Noura</h1>
+        <p>Dança do Ventre, Danças Folclóricas Árabes e mais.</p>
+        <div class="stars" id="stars"></div>
+    </header>
